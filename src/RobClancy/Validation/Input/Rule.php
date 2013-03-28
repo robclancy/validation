f32bbdce2d38bb4d9e1e880eed2d1b778784914c
Robbo <robert@mercenarydesign.net>
2013-03-28 21:57:05 +1100

    refactor, fixes etc

--- src/RobClancy/Validation/Input/Rule.php
+++
@@ -1,7 +0,0 @@
-<?php namespace RobClancy\Validation\Input;
-
-use RobClancy\ValidationRules\Rule as Base;
-
-class Rule extends Base {
-
-}
--- src/RobClancy/Validation/Validator/Validator.php
+++
@@ -1,8 +0,0 @@
-<?php namespace RobClancy\Validation\Validator;
-
-interface Validator {
-	
-	public function check($input, $rules);
-	
-	public function getErrors();
-}
